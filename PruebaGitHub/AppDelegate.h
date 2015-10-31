3b675c025283f036a52d1b98e2844403aa9e515a
Quiñones Luna Victoria Alonso <alonso.quinones@ferreyros.com.pe>
2015-10-31 18:12:38 -0500

    Initial Commit

---
+++ PruebaGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaGitHub
+//
+//  Created by Anto & Alon on 31/10/15.
+//  Copyright © 2015 Anto & Alon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaGitHub
+//
+//  Created by Anto & Alon on 31/10/15.
+//  Copyright © 2015 Anto & Alon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
